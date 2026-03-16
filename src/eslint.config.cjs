2f41bf4a9008a1002d1b0e541b2c9a7b46ff995b
Chad Yates <cyates@dynfxdigital.com>
2026-03-15 21:23:05 -0700

    build: upgrade workspace tooling dependencies

---
+++ src/eslint.config.cjs
@@ -0,0 +1,72 @@
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+
+module.exports = [
+    {
+        ignores: ['out/**', 'dist/**', '**/*.d.ts'],
+    },
+    {
+        files: ['src/**/*.ts'],
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 2022,
+            sourceType: 'module',
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+        },
+        rules: {
+            ...tsPlugin.configs.recommended.rules,
+            '@typescript-eslint/naming-convention': [
+                'warn',
+                {
+                    selector: 'default',
+                    format: ['camelCase'],
+                },
+                {
+                    selector: 'variable',
+                    format: ['camelCase', 'UPPER_CASE'],
+                },
+                {
+                    selector: 'parameter',
+                    format: ['camelCase'],
+                    leadingUnderscore: 'allow',
+                },
+                {
+                    selector: 'memberLike',
+                    modifiers: ['private'],
+                    format: ['camelCase'],
+                    leadingUnderscore: 'allow',
+                },
+                {
+                    selector: 'typeLike',
+                    format: ['PascalCase'],
+                },
+                {
+                    selector: 'enumMember',
+                    format: ['PascalCase'],
+                },
+                {
+                    selector: 'property',
+                    format: ['camelCase', 'snake_case'],
+                    leadingUnderscore: 'allow',
+                },
+                {
+                    selector: 'import',
+                    format: ['camelCase', 'PascalCase'],
+                },
+            ],
+            semi: ['warn', 'always'],
+            curly: 'warn',
+            eqeqeq: 'warn',
+            'no-throw-literal': 'warn',
+        },
+    },
+    {
+        files: ['src/test/**/*.ts'],
+        rules: {
+            '@typescript-eslint/naming-convention': 'off',
+            '@typescript-eslint/no-require-imports': 'off',
+        },
+    },
+];
